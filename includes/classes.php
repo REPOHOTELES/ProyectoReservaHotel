314d0f790977ed119b91dffb80b3670ef6c14bb7
JulianC06 <87028380+JulianC06@users.noreply.github.com>
2022-08-06 17:27:39 -0500

    PRH-31 Clases que se usarán comunmente

---
+++ includes/classes.php
@@ -0,0 +1,10 @@
+<?php 
+include 'database.php';
+include 'person.php';
+include 'user.php';
+include 'holder.php';
+include 'consult.php';
+include 'enterprise.php';
+include 'reservation.php';
+include 'room.php';
+?>
