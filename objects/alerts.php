a2e977ed7b044d22cc1eeb932ded2d6a3832b380
JulianC06 <87028380+JulianC06@users.noreply.github.com>
2022-08-06 20:12:50 -0500

    PRH-38 Creando las alertas
    Se hace llamado a las alertas dependiendo la situación.

---
+++ objects/alerts.php
@@ -0,0 +1,29 @@
+<!--Contiene el bloque correspondiente a las alertas emitidas por el sistema luego de presentarse algún evento ya sea inesperado o controlado por el usuario-->
+
+<link rel="stylesheet" type="text/css" href="../css/alerts.css">
+<div id="alerts" class="col-3">
+
+    <div>
+        <div id="alert-d" class="alert danger">
+            <span onclick="hideAlert(this);" class="closebtn">&times;</span>  
+            <strong>¡Error!</strong> 
+            <p></p>
+        </div>
+        <div id="alert-s" class="alert success">
+            <span onclick="hideAlert(this);" class="closebtn">&times;</span>  
+            <strong>¡Procedimiento exitoso!</strong> 
+            <p></p>
+        </div>
+        <div id="alert-i" class="alert info">
+            <span onclick="hideAlert(this);" class="closebtn">&times;</span>  
+            <strong>Información!</strong> 
+            <p></p>
+        </div>
+    
+        <div id="alert-w" class="alert warning">
+            <span onclick="hideAlert(this);" class="closebtn">&times;</span>  
+            <strong>¡Precaución!</strong> 
+            <p><p>
+        </div>
+    </div>
+</div>
