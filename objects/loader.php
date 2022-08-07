a09aba5df7c00ce764fd535ffb05fe3078be9529
JulianC06 <87028380+JulianC06@users.noreply.github.com>
2022-08-06 20:27:00 -0500

    PRH-36 Creando el loader de las paginas
    Verificar que el logo se llame así, de lo contrario corregir.

---
+++ objects/loader.php
@@ -0,0 +1,21 @@
+<div class="loader">
+</div>
+<style type="text/css">
+    .loader{
+        position: fixed;
+        left: 0px;
+        top: 0px;
+        width: 100%;
+        height: 100%;
+        z-index: 100; 
+        background:url('../res/img/logoA.png') center no-repeat #fff; 
+        background-size: 12.5%;
+        align-content: center;
+    }
+</style>
+
+<script type="text/javascript">
+    $(document).ready(function(){
+        $(".loader").fadeOut("slow");
+    });
+</script>
